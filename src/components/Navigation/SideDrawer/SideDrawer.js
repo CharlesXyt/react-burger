43e5f294afa7aa69f4c82976c73e6c6300bd18ec
charles <charles.yitong@gmail.com>
2019-12-05 16:22:12 +1100

    toolbar

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Logo from '../../Logo/Logo'
+import NavigationItems from '../NavigationItems/NavigationItems'
+import classes from './SideDrawer.module.css'
+import Backdrop from '../../UI/Backdrop/Backdrop'
+
+
+const sideDrawer =(props) => {
+
+    const attachedClass = [classes.SideDrawer]
+    if(!props.open){
+        attachedClass.push(classes.Close)
+    }
+
+    return (
+        <React.Fragment>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClass.join(" ")}>
+                <div className={classes.Logo}>
+                    <Logo/>
+                </div>
+                <nav>
+                    <NavigationItems/>
+                </nav>
+            </div>
+        </React.Fragment>
+        
+    );
+}
+
+
+export default sideDrawer;
