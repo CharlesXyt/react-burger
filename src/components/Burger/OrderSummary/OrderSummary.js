6f18d9626b5e1d0d24186e560680c61d37193843
charles <charles.yitong@gmail.com>
2019-12-05 16:40:51 +1100

    add shouldcomponentupdate

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,32 +1,35 @@
-import React from 'react'
+import React ,{Component}from 'react'
 import Button from '../../UI/Button/Button'
 
 
-const orderSummary = (props) => {
+class OrderSummary extends Component{
 
-    const ingredientSummary = Object.keys(props.ingredients).map(igkey =>{
-        return (
-    <li key={igkey}><span style={{textTransform:'capitalize'}}>{igkey}</span>:{props.ingredients[igkey]}</li>
+
+
+    render(){
+        const ingredientSummary = Object.keys(this.props.ingredients).map(igkey =>{
+            return (
+                <li key={igkey}><span style={{textTransform:'capitalize'}}>{igkey}</span>:{this.props.ingredients[igkey]}</li>
+            )
+        })
+        return(
+            <React.Fragment>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price:{this.props.totalPrice.toFixed(2)}</strong></p>
+                <p>continue to check out?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCancelled}>Cancel</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>Continue</Button>
+            </React.Fragment>
         )
-    })
-    return (
-        <React.Fragment>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price:{props.totalPrice.toFixed(2)}</strong></p>
-            <p>continue to check out?</p>
-            <Button btnType="Danger" clicked={props.purchaseCancelled}>Cancel</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>Continue</Button>
-        </React.Fragment>
+
+    }
+}
+
+    
 
 
-    )
-
-
-};
-
-
-export default orderSummary;
+export default OrderSummary;
