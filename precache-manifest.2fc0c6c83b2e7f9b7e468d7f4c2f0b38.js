208919133f4fb88ba850b4eb51ee900b13c4a3a0
charlesxyt <charles.yitong@gmail.com>
2020-01-20 16:57:07 +1100

    Updates

---
+++ precache-manifest.2fc0c6c83b2e7f9b7e468d7f4c2f0b38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95bf9e3a7ff4af9f86902b67d5126ca0",
+    "url": "/burger/index.html"
+  },
+  {
+    "revision": "87fd6ed71fd5f93dd2e5",
+    "url": "/burger/static/css/main.807d8d43.chunk.css"
+  },
+  {
+    "revision": "19e689fd255611d12ba6",
+    "url": "/burger/static/js/2.5f7cfd16.chunk.js"
+  },
+  {
+    "revision": "19e4dab51200fc3aeb20264502017846",
+    "url": "/burger/static/js/2.5f7cfd16.chunk.js.LICENSE"
+  },
+  {
+    "revision": "87fd6ed71fd5f93dd2e5",
+    "url": "/burger/static/js/main.f1c042ed.chunk.js"
+  },
+  {
+    "revision": "d4390a2908c1cac1ad60",
+    "url": "/burger/static/js/runtime-main.b5950f3a.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger/static/media/burger-logo.b8503d26.png"
+  }
+]);
