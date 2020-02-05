1163cbbdf62c62a9009f18a00778c78bceaf779a
charlesxyt <charles.yitong@gmail.com>
2020-02-06 10:30:03 +1100

    change to react-hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import {connect} from 'react-redux'
 import * as actions from './components/store/actions/index'
 import Layout from './components/hoc/Layout/Layout'
@@ -15,42 +15,41 @@ const CheckOut = React.lazy(() => import('./containers/CheckOut/CheckOut'))
 
 
  
-class App extends React.Component {
+const App = props => {
   componentDidMount(){
     this.props.onTryAutoSignup();
   }
 
-  render(){
-    let routes = (
+  let routes = (
+    <Switch>
+      <Route path="/auth" component={Auth} />
+      <Route path="/" exact component={BurgerBuilder} />
+      <Redirect to="/"/>
+    </Switch>
+    
+  )
+
+  if(this.props.isAuthenticated){
+    routes = (
       <Switch>
+        <Route path="/checkout" component={CheckOut} />
+        <Route path="/orders" component={Orders} />
+        <Route path="/logout" component={Logout} />
         <Route path="/auth" component={Auth} />
         <Route path="/" exact component={BurgerBuilder} />
         <Redirect to="/"/>
       </Switch>
-      
     )
-
-    if(this.props.isAuthenticated){
-      routes = (
-        <Switch>
-          <Route path="/checkout" component={CheckOut} />
-          <Route path="/orders" component={Orders} />
-          <Route path="/logout" component={Logout} />
-          <Route path="/auth" component={Auth} />
-          <Route path="/" exact component={BurgerBuilder} />
-          <Redirect to="/"/>
-        </Switch>
-      )
-    }
-
-    return (
-      <div>
-          <Layout>
-            {routes}
-          </Layout>
-      </div>
-    );
   }
+
+  return (
+    <div>
+        <Layout>
+          {routes}
+        </Layout>
+    </div>
+  );
+
   
 }
 
