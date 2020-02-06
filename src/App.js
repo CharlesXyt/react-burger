3774a6386cb6bcc6f1aa6de05ad1ad19b3477653
charlesxyt <charles.yitong@gmail.com>
2020-02-06 14:31:07 +1100

    fix price bug

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useEffect} from 'react';
+import React from 'react';
 import {connect} from 'react-redux'
 import * as actions from './components/store/actions/index'
 import Layout from './components/hoc/Layout/Layout'
@@ -15,42 +15,42 @@ const CheckOut = React.lazy(() => import('./containers/CheckOut/CheckOut'))
 
 
  
-const App = props => {
+class App extends React.Component{
   componentDidMount(){
     this.props.onTryAutoSignup();
   }
 
-  let routes = (
-    <Switch>
-      <Route path="/auth" component={Auth} />
-      <Route path="/" exact component={BurgerBuilder} />
-      <Redirect to="/"/>
-    </Switch>
-    
-  )
-
-  if(this.props.isAuthenticated){
-    routes = (
+  render(){
+    let routes = (
       <Switch>
-        <Route path="/checkout" component={CheckOut} />
-        <Route path="/orders" component={Orders} />
-        <Route path="/logout" component={Logout} />
         <Route path="/auth" component={Auth} />
         <Route path="/" exact component={BurgerBuilder} />
         <Redirect to="/"/>
       </Switch>
+      
     )
-  }
-
-  return (
-    <div>
-        <Layout>
-          {routes}
-        </Layout>
-    </div>
-  );
-
   
+    if(this.props.isAuthenticated){
+      routes = (
+        <Switch>
+          <Route path="/checkout" component={CheckOut} />
+          <Route path="/orders" component={Orders} />
+          <Route path="/logout" component={Logout} />
+          <Route path="/auth" component={Auth} />
+          <Route path="/" exact component={BurgerBuilder} />
+          <Redirect to="/"/>
+        </Switch>
+      )
+    }
+  
+    return (
+      <div>
+          <Layout>
+            {routes}
+          </Layout>
+      </div>
+    );
+  }
 }
 
 
